ebe33da75cc4f6f6ac6769a9e74aab4b07323963
khadra mohamed <khadramohamed@live.co.uk>
2022-04-02 12:26:26 +0100

    hello

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,10 @@
-const numberToReversedDigits = number => {};
+const numberToReversedDigits = number => {
+  const numToString = number.toString();
+  const newArray = numToString.split('').reverse;
+  const finalResults = [];
+  newArray.forEach(element => finalResults.push(Number(element)));
+
+  return finalResults;
+};
 
 module.exports = numberToReversedDigits;
